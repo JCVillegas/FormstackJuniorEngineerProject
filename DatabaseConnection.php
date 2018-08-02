46e2f3114b9d702c89db6c9fe337b7c6f5cc55b2
JC Villegas <juan.villegas@formstack.com>
2018-08-02 11:55:12 -0500

    Database PDO

---
+++ DatabaseConnection.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace JCVillegas\JuniorProject;
+
+/*
+*   @ Database class
+*/
+class DatabaseConnection
+{
+    private $databaseHandler;
+    private $sql;
+    /**
+     *   @ Class database constructor
+     */
+    public function __construct()
+    {
+        $dataSource = 'mysql:host='.DatabaseConfig::DB_HOST.';dbname='.DatabaseConfig::DB_NAME;
+
+        $options = array(
+            \PDO::ATTR_PERSISTENT => true,
+            \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
+            \PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
+        );
+        $this->databaseHandler = new \PDO($dataSource, DatabaseConfig::DB_USER, DatabaseConfig::DB_PASS, $options);
+    }
+    /**
+     * Receives a query
+     * @param  string $query
+     * @return void
+     */
+    public function query($query)
+    {
+        $this->sql = $this->databaseHandler->prepare($query);
+    }
+
+    /**
+     *
+     * @param  mixed $param Identifier
+     * @param  mixed $value Variable name
+     * @param  mixed $type  Data type
+     * @return void
+     */
+    public function bind($param, $value, $type)
+    {
+        $this->sql->bindValue($param, $value, $type);
+    }
+
+    /**
+     * Executes query
+     */
+    public function execute()
+    {
+        return $this->sql->execute();
+    }
+
+    /**
+     * Returns database result
+     * @return array
+     */
+    public function resultSet()
+    {
+        $this->execute();
+
+        return $this->sql->fetchAll(\PDO::FETCH_ASSOC);
+    }
+}
