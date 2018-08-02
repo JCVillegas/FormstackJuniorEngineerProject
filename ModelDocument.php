74a44e1ce2fa57fd0a732d8955279c989f59919a
JC Villegas <juan.villegas@formstack.com>
2018-08-02 18:15:38 -0500

    Added save method to model

--- ModelDocument.php
+++ ModelDocument.php
@@ -27,4 +27,38 @@ class ModelDocument
         return $rows;
     }
 
+    /**
+     * Creates or update documents
+     * @param  array $userData
+     * @return array $result
+     * @throws \Exception
+     */
+    public function saveDocument($userData)
+    {
+
+
+        $userData['documentName'] = !empty($userData['documentName']) ? trim(substr($userData['documentName'], 0, 100)) : '';
+
+        foreach (array_combine($userData['key'], $userData['value'])  as $key => $value) {
+            $checkKey   = !empty($key) ? trim(substr($key, 0, 100)) : '';
+            $checkValue = !empty($value) ? trim(substr($value, 0, 100)) : '';
+
+            if ($checkKey == '' || $checkValue == '' || $userData['documentName'] == '') {
+                throw new \Exception('Incomplete document data.');
+            }
+        }
+
+        $jsonKeyValue = json_encode(array_combine($userData['key'], $userData['value']));
+        $this->database->query('INSERT INTO  '.DatabaseConfig::DB_TABLE.' (name, keyvalues, created, updated, exported) 
+                  VALUES (:name, :keyvalues, :created, :updated, :exported)');
+
+        $this->database->bind(':name', $userData['documentName'], \PDO::PARAM_STR);
+        $this->database->bind(':keyvalues', $jsonKeyValue, \PDO::PARAM_STR);
+        $this->database->bind(':created', date('Y-m-d H:i:s'), \PDO::PARAM_STR);
+        $this->database->bind(':updated', '0000-00-00 00:00:00', \PDO::PARAM_STR);
+        $this->database->bind(':exported', '0000-00-00 00:00:00', \PDO::PARAM_STR);
+
+        $result = $this->database->execute();
+        return $result;
+    }
 }
