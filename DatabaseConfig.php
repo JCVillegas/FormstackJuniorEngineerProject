fbdaad6e237cba15349949758f8e0414f4e6d3d3
JC Villegas <juan.villegas@formstack.com>
2018-08-02 11:23:49 -0500

    Added connection constants

---
+++ DatabaseConfig.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace JCVillegas\JuniorProject;
+
+/**
+ *   @ Database mysql constants
+ */
+
+class DatabaseConfig
+{
+    const DB_HOST  = '192.168.64.2';
+    const DB_USER  = 'my_app';
+    const DB_PASS  = 'secret';
+    const DB_NAME  = 'my_app';
+    const DB_TABLE = 'document';
+}
