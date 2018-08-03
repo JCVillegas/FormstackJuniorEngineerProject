2221173f8fb83f0fbc8ed45252613d22581263af
JC Villegas <juan.villegas@formstack.com>
2018-08-03 11:04:50 -0500

    Updated view form to be able to update document data

--- ViewDocumentEdit.php
+++ ViewDocumentEdit.php
@@ -21,16 +21,38 @@ class ViewDocumentEdit
         $this->viewFooter = $viewFooter;
     }
 
-    public function show($userData = array(), $error = '', $update = false)
+    public function show($documentData = array(), $error = '', $update = false)
     {
+        $id           = !empty($documentData['id']) ? (int) $documentData['id'] : 0;
+        $documentName = !empty($documentData['name']) ? trim(substr($documentData['name'], 0, 100)) : '';
+        $created      = !empty($documentData['created']) ? trim(substr($documentData['created'], 0, 100)) : '';
+        $exported     = !empty($documentData['exported']) ? trim(substr($documentData['exported'], 0, 100)) : '';
+        $keyValues    = json_decode($documentData['keyvalues'], true);
+
         $createDocumentForm = "<form action='index.php?operation=saveDocument' method='post'>";
         $createDocumentForm.= "<table class='table table-bordered' id='dynamic_field'>";
         $createDocumentForm.= "<tr>";
-        $createDocumentForm.= "<td><input type='text' name='documentName' placeholder='Enter a document name'/></td>";
+        $createDocumentForm.= "<td><input type='text' name='documentName' value='".htmlentities($documentName)."' placeholder='Enter a document name'/></td>";
         $createDocumentForm.= "</tr>";
-        $createDocumentForm.= "<tr>";
-        $createDocumentForm.= "<td><input type='text' name='key[]' placeholder='Enter a KEY'/></td>";
-        $createDocumentForm.= "<td><input type='text' name='value[]' placeholder='Enter a VALUE'/></td>";
+
+        if ($update) {
+            $createDocumentForm .= "<input type='hidden' name='updateForm' value='1'> <br>";
+            $createDocumentForm .= "<input type='hidden' name='id' value='" . htmlentities($id) . "'> <br>";
+            $createDocumentForm .= "<input type='hidden' name='created' value='" . htmlentities($created) . "'> <br>";
+            $createDocumentForm .= "<input type='hidden' name='exported' value='" . htmlentities($exported) . "'> <br>";
+
+            foreach ($keyValues as $key=>$value)
+            {
+                $createDocumentForm.= "<tr>";
+                $createDocumentForm.= "<td><input type='text' name='key[]' value='" . htmlentities($key) . "' placeholder='Enter a KEY'/></td>";
+                $createDocumentForm.= "<td><input type='text' name='value[]' value='" . htmlentities($value) . "' placeholder='Enter a VALUE'/></td>";
+                $createDocumentForm.= "</tr>";
+            }
+        }
+        else{
+            $createDocumentForm.= "<td><input type='text' name='key[]' placeholder='Enter a KEY'/></td>";
+            $createDocumentForm.= "<td><input type='text' name='value[]' placeholder='Enter a VALUE'/></td>";
+        }
         $createDocumentForm.= "<td><button type='button' name='add' id='add'>Add More</button></td>";
         $createDocumentForm.= "</tr>";
         $createDocumentForm.= "<tr>";
@@ -38,9 +60,6 @@ class ViewDocumentEdit
         $createDocumentForm .= "<br><a href='index.php?operation=readDocuments'>Go back to list documents</a></td>";
         $createDocumentForm.= "</tr>";
         $createDocumentForm.= "</table>";
-        if ($update) {
-            $createDocumentForm .= "<input type='hidden' name='updateForm' value='1'> <br>";
-        }
         $createDocumentForm.= '</form>';
 
         $this->viewHeader->show();
